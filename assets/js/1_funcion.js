48382091c4cf1b88ebdb3b4489841159699704e7
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-01-18 13:08:22 -0300

    Ejercicio 1 y 2: listos

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,13 @@
+
+/* Función Normal */
+function example (a,b,c){
+    return a+b+c;
+}
+
+/* Función de expresion */
+
+let ejemplo = function (a,b,c){
+    return a+b+c
+}
+
+console.log(ejemplo(1,2,3));
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,10 @@
+/* Función de expresión */
+let suma = function (a,b){
+    return a + b;    
+}
+
+/* Funcion arrow de 1 sola línea */
+
+let sum = (a,b)=> a + b
+
+console.log(sum(2,3));
